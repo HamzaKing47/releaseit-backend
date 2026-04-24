fe2e0ff74d2821c4a8000085312c4b9a5538fe35
HamzaKing47 <hamzashahid1535@gmail.com>
2026-04-24 15:28:08 +0500

    fix oauth routes

--- server.js
+++ server.js
@@ -64,3 +64,7 @@ const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`🚀 Server running on port ${port}`);
 });
+
+server.get("/test", (req, res) => {
+  res.send("AUTH ROUTE ACTIVE");
+});
